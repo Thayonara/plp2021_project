0069307ef9725bd5e29793d764fb86a399ca282a
Thayonara <tpa@cin.ufpe.br>
2021-06-20 12:16:20 -0300

    add actions to allow FeatureName Declarations

--- src/implementations/FeatureNameDeclaration.java
+++ src/implementations/FeatureNameDeclaration.java
@@ -1,11 +1,11 @@
 package implementations;
 
-public class FeatureNameDeclation {
+public class FeatureNameDeclaration {
     protected Id featureName;
     protected Id extendedNode;
     protected Types nodeType;
 
-    public FeatureNameDeclation(Id featureName, Id extendedNode, Types nodeType){
+    public FeatureNameDeclaration(Id featureName, Id extendedNode, Types nodeType){
         this.featureName = featureName;
         this.extendedNode = extendedNode;
         this.nodeType = nodeType;
