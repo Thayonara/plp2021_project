a0d9065b572f57697d27907cf1bb80bc17c2467f
Reydne <reydne.bruno@gmail.com>
2021-07-22 20:03:25 -0300

    Check method implemented

--- src/commands/OneFactor.java
+++ src/commands/OneFactor.java
@@ -1,14 +1,40 @@
 package commands;
 
-public class OneFactor implements Commands {
+import exceptions.PreviouslyDeclaredFNException;
+import exceptions.PreviouslyDeclaredPLException;
+import exceptions.UndeclaredFNException;
+import exceptions.UndeclaredPLException;
+import implementations.Id;
+import implementations.PLDeclaration;
+import implementations.ProductDeclaration;
+import memory.CompilationEnvironment;
+
+public class OneFactor {
+
+    private PLDeclaration plDeclaration;
+    private ProductDeclaration productDeclaration;
 
     public void OneFactor(){
+        this.TypeCheckPL();
+        this.TypeCheckProduct();
+
+
+        int arr[] = {1, 2, 3, 4};
+        System.out.println("Before update" + arr[2]);
+        arr[2] = 9;//updating the value
+        System.out.println("After update" + arr[2]);
+
     }
 
-    public void checaPL(){
+    public boolean TypeCheckPL(CompilationEnvironment compilationEnvironment) throws PreviouslyDeclaredPLException, UndeclaredPLException, PreviouslyDeclaredFNException, UndeclaredFNException {
+        boolean pl;
+        pl = plDeclaration.TypeCheck(compilationEnvironment) ;
+        return pl;
     }
 
-    public boolean checaProduto(){
-         return true;
+    public boolean TypeCheckProduct(CompilationEnvironment compilationEnvironment) {
+        boolean product;
+        product = plDeclaration.TypeCheck(compilationEnvironment) ;
+        return product;
     }
 }
--- src/commands/SimplifiedOneFactor.java
+++ src/commands/SimplifiedOneFactor.java
@@ -1,16 +1,31 @@
 package commands;
 
-public class SimplifiedOneFactor implements Commands{
+import exceptions.PreviouslyDeclaredFNException;
+import exceptions.PreviouslyDeclaredPLException;
+import exceptions.UndeclaredFNException;
+import exceptions.UndeclaredPLException;
+import implementations.PLDeclaration;
+import implementations.ProductDeclaration;
+import memory.CompilationEnvironment;
+
+public class SimplifiedOneFactor {
+
+    private PLDeclaration plDeclaration;
+    private ProductDeclaration productDeclaration;
 
     public void SimplifiedOneFactor(){
     }
 
-    public void checaPL(){
+    public boolean TypeCheckPL(CompilationEnvironment compilationEnvironment) throws PreviouslyDeclaredPLException, UndeclaredPLException, PreviouslyDeclaredFNException, UndeclaredFNException {
+        boolean pl;
+        pl = plDeclaration.TypeCheck(compilationEnvironment) ;
+        return pl;
     }
 
-    public boolean checaProduto(){
-        return true;
+    public boolean TypeCheckProduct(CompilationEnvironment compilationEnvironment) {
+        boolean product;
+        product = plDeclaration.TypeCheck(compilationEnvironment) ;
+        return product;
     }
-
 }
 
