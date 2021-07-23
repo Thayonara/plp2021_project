630f49d11df46b50f6e441f247561e6340ca0cfa
Reydne <reydne.bruno@gmail.com>
2021-07-22 22:55:16 -0300

    Add structure to t-way

---
+++ src/commands/Tway.java
@@ -0,0 +1,31 @@
+package commands;
+
+import exceptions.PreviouslyDeclaredFNException;
+import exceptions.PreviouslyDeclaredPLException;
+import exceptions.UndeclaredFNException;
+import exceptions.UndeclaredPLException;
+import implementations.PLDeclaration;
+import implementations.ProductDeclaration;
+import memory.CompilationEnvironment;
+
+public class Tway {
+
+    private PLDeclaration plDeclaration;
+    private ProductDeclaration productDeclaration;
+
+    public void tway(){
+    }
+    public boolean TypeCheckPL(CompilationEnvironment compilationEnvironment) throws PreviouslyDeclaredPLException, UndeclaredPLException, PreviouslyDeclaredFNException, UndeclaredFNException {
+        boolean pl;
+        pl = plDeclaration.TypeCheck(compilationEnvironment) ;
+        return pl;
+    }
+
+    public boolean TypeCheckProduct(CompilationEnvironment compilationEnvironment) {
+        boolean product;
+        product = plDeclaration.TypeCheck(compilationEnvironment) ;
+        return product;
+    }
+}
+
+
